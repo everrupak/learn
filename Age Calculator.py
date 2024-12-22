3f329eefa28de0af5cbd89f598a50ff63959487a
everrupak <41101609+everrupak@users.noreply.github.com>
2024-12-22 20:14:21 +0545

    Delete Age Calculator.py

--- Age Calculator.py
+++
@@ -1,5 +0,0 @@
-# Age Calculator
-birth_year = int(input('Enter birth year: '))
-current_year = int(input('Enter current year: '))
-age = str(current_year - birth_year)
-print('You are ' + age + ' years old
