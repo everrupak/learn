970d5045045cf36299c4bd1a5fdbb520d661231b
everrupak <41101609+everrupak@users.noreply.github.com>
2024-12-22 21:20:39 +0545

    Add files via upload

---
+++ Formatted String.py
@@ -0,0 +1,6 @@
+# Formatted String
+first_name = 'John'
+last_name = 'Smith'
+msg = f'{first_name} [{last_name}] is a coder.'
+print(msg)
+
---
+++ String Methods.py
@@ -0,0 +1,11 @@
+# String Methods
+# print and len are functions
+# upper, lower, find, replace are methods
+course = 'Python for Beginners'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find('n'))
+print(course.replace('Beginners', 'Experts'))
+print(course.replace('n', 'N'))
+print('Python' in course)
