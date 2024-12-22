ccc611bb5d8fe186b156c3ee769cc882ccf84b03
everrupak <41101609+everrupak@users.noreply.github.com>
2024-12-22 20:15:51 +0545

    Age Calculator

---
+++ Age Calculator.py
@@ -0,0 +1,5 @@
+# Age Calculator
+birth_year = int(input('Enter birth year: '))
+current_year = int(input('Enter current year: '))
+age = current_year - birth_year
+print('You are ' + str(age) + ' years old.')
