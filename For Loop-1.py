1c2b6feb2f9a8c7a50b02549a80ad5cfde82803d
everrupak <41101609+everrupak@users.noreply.github.com>
2025-01-07 09:42:32 +0545

    Add files via upload

---
+++ For Loop-1.py
@@ -0,0 +1,21 @@
+# For loop is used to iterate over each character in a string and then do something with it.
+for item in 'Python':
+    print(item)
+
+# List case
+for item in ['John', 'Harry', 'Rupak']:
+    print(item)
+
+
+# List case - Numbers
+for item in [1,2,3,4,5]:
+    print(item)
+
+# Range case - 3 to 9
+for item in range(3, 10):
+    print(item)
+
+# Range case - 3 to 9 at 2 intervals
+for item in range(3, 10, 2):
+    print(item)
+
---
+++ While Loop-1.py
@@ -0,0 +1,5 @@
+i = 1
+while 5 >= i:
+    print('*' * i)
+    i += 1
+print('Done')
